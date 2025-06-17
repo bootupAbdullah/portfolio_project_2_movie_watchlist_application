3c3e418c938a0ac371f3478545c3c5750de37e9b
Abdullah Durrani <134801570+bootupAbdullah@users.noreply.github.com>
2025-06-17 14:11:34 -0400

    pushing up changed to search

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -1,82 +1,89 @@
-import { useState, useEffect } from 'react'
-import SearchList from '/src/components/Search/SearchList.jsx'
+import { useState } from 'react';
+import SearchList from '/src/components/Search/SearchList.jsx';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
-import './Search.css'
+import './Search.css';
 
+const Search = ({ fetchDataForMovies }) => {
+  const [searchTitle, setSearchTitle] = useState('');
+  const [foundTitle, setFoundTitle] = useState('');
+  const API = import.meta.env.VITE_OMDB_API_KEY;
+
+  const handleChange = (event) => {
+    setSearchTitle(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let response = await fetch(
+      `https://www.omdbapi.com/?apikey=${API}&i=${searchTitle}`
+    );
+    let JSONdata = await response.json();
+    console.log(JSONdata);
+    setFoundTitle(JSONdata);
+  };
+
+  return (
+    <>
+      <div id="searchDiv">
+        <h1>Search</h1>
+        <Navbar id="searchFormBar" className="justify-content-between">
+          <Form onSubmit={handleSubmit} id="searchForm">
+            Please enter an imdbID of a movie you would like to add to your Watchlist:
+            <br />
+            <br />
+            <Row>
+              <Form.Control
+                type="text"
+                placeholder="imdbID"
+                id="searchFormControl"
+                onChange={handleChange}
+              />
+              <Button id="searchButton" type="submit">
+                Get my movie
+              </Button>
+            </Row>
+          </Form>
+        </Navbar>
+        <SearchList fetchDataForMovies={fetchDataForMovies} foundTitle={foundTitle} />
+      </div>
+    </>
+  );
+};
+
+export default Search;
+
+// ------------------- graveyard -------------------
 
 // const dotenv = require("dotenv")
 // dotenv.config()
 
-const Search = ({fetchDataForMovies}) => {
-    //2.) add state to our Home. create a variable named title initialized by the hook useState. NOTE THIS WILL ACTUALLY BE AN IMDBID
-    const [ searchTitle, setSearchTitle] = useState('')
-    //9.) **NOW NOT IN USE add variable to handle the response data and display. i am hard coding this into the component for now just to get and idea of how to access the values, then i will map onto a component card to display
-    const  [foundTitle, setFoundTitle] = useState('') //10.) the movie object found
-    const API = '5b053eb2'
-    //3.) create a handler that will respond to change events when the text field is typed in. add onChange to the input
-    const handleChange = (event) => {
-        setSearchTitle(event.target.value)
-    }
+// const fetchDataForMovies = props.fetchDataForMovies //add to pass
 
-//const fetchDataForMovies = props.fetchDataForMovies//add to pass
-    //4.) Get an api key and write a function for submitting that will request to the API
-    //const API = OMDb_API ** need to figure out how to incorporate requiring dotenv so i can access this through .env instead of exposing key
-   //props.fetchDataForMovies();
+// const API = OMDb_API ** need to figure out how to incorporate requiring dotenv so i can access this through .env instead of exposing key
 
-    const handleSubmit = async (event) => {
-        event.preventDefault() //5.) prevents us from taking us to a different page
-        let response = await fetch( //6.) use fetch to make an AJAX request to the outside API. 
-            `https://www.omdbapi.com/?apikey=${API}&i=${searchTitle}`
-        )
-        let JSONdata = await response.json(); //7.) return the response as a javascript object
-        console.log(JSONdata);
-        setFoundTitle(JSONdata);
-    }
-//props.fetchDataForMovies()
-    return (
-    // 1.) create a form where the user can input data. We will add the movie title to search by title
-    // 8.) add onSubmit={handleSubmit} on the form to link to the handleSubmit fn
-    //12.) **NOW NOT IN USE added ternary statement to conditionally render the movie cards
-    //13.) importing HomeSearchList component and passing found title to render as a card
-    <>
-    <div id="searchDiv">
-    <h1>Search</h1>
-    <Navbar id="searchFormBar" className="justify-content-between">
-    <Form onSubmit={handleSubmit} id="searchForm">
-        Please enter an imdbID of a movie you would like to add to your Watchlist: <br/>
-        <br/>
-        <Row>
-        {/* <Form.Control
-              type="text"
-              placeholder="Search"
-              className=" mr-sm-2"
-            /> */}
-          {/* <Col xs="auto"> */}
-        {/* <input type="text" onChange={handleChange}/> */}
-        <Form.Control type="text" placeholder="imdbID" id="searchFormControl" onChange={handleChange}/>
-        {/* </Col> */}
-        {/* <Col xs="auto"> */}
-        <Button id= "searchButton" type="submit">Get my movie</Button>
-        {/* </Col> */}
-        </Row>
-    </Form>
-    </Navbar>
-    <SearchList fetchDataForMovies={fetchDataForMovies} foundTitle={foundTitle}/> 
-    </div>
+// props.fetchDataForMovies()
 
-    {/* <form onSubmit={handleSubmit}>
-        Please enter a movie title you would like to add to your Watchlist: <br/>
-        <input type="text" onChange={handleChange}/>
-        <input type="submit" value="Get my moive"/>
-    </form> */}
-  
-    </>
-)
-}
+/*
+<form onSubmit={handleSubmit}>
+  Please enter a movie title you would like to add to your Watchlist: <br/>
+  <input type="text" onChange={handleChange}/>
+  <input type="submit" value="Get my moive"/>
+</form>
+*/
 
-export default Search
-
-
+/*
+<Form.Control
+  type="text"
+  placeholder="Search"
+  className=" mr-sm-2"
+/>
+<Col xs="auto">
+<input type="text" onChange={handleChange}/>
+</Col>
+<Col xs="auto">
+<Button id= "searchButton" type="submit">Get my movie</Button>
+</Col>
+*/
